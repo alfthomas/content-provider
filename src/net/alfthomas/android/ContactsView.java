bfb5608990893bd82beaa8dbabac4e2400f90857
alfthomas <alf.thomas.nilsen@gmail.com>
2012-01-01 20:43:27 +0100

    Created project

---
+++ src/net/alfthomas/android/ContactsView.java
@@ -0,0 +1,13 @@
+package net.alfthomas.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ContactsView extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
